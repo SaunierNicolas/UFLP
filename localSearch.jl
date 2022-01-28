0aaefe8c37bcc864fd2ee7cc5ba85665138ced31
AchillePoirier <achille.poirier@mail.fr>
2022-01-28 03:37:09 +0100

    localSearch avec somme pondéré
    ça marche presque :/

--- localSearch.jl
+++ localSearch.jl
@@ -1,32 +1,194 @@
 
-function localSearch(i,j,y,f,c)
-    #kp-exchange
-    return (pairingClient(i,j,y,c),0)
+function localSearch(i,j,x,y,f,c,l_1,l_2)
+    
+    y_new = deepcopy(y)
+    x_new = deepcopy(x)
+    x_new = pairingClient(i,j,y,c,l_1,l_2)
+
+    println("kp 2-1")
+    println(zValue(i,j,x_new,y_new,f[1],c[1]),zValue(i,j,x_new,y_new,f[2],c[2]),y_new)
+    change,y_new = kp_2_1(i,j,x,y_new,f,c,l_1,l_2)
+    while change == true
+        println(zValue(i,j,x_new,y_new,f[1],c[1]),zValue(i,j,x_new,y_new,f[2],c[2]),y_new)
+        change,y_new = kp_2_1(i,j,x,y_new,f,c,l_1,l_2)
+    end
+
+    x_new = pairingClient(i,j,y_new,c,l_1,l_2)
+    
+
+    return (x_new,y_new)
 end
 
 #détermine la meilleure affectation client-depot (depot = site ouvert)
-function pairingClient(n_client,n_site,y,c)
+function pairingClient(n_client,n_site,y,c,l_1,l_2)
     
     #initialisation de x 
-    x = Array{Int64}(undef,n_client,n_site)
+    x = Vector{Vector{Int64}}(undef,0)
     for i = 1:n_client
-        for j = 1:n_site
-            x[i,j] = 0
-        end
+        push!(x,zeros(n_site))
+        
     end
 
-    #recherche du cout minimum
+    #Recherche du cout minimum.
     for i = 1:n_client
         indiceDepot_minCost = 1
         for j = 1:n_site
             if y[j] == 1
-                if c[i][j] < c[i][indiceDepot_minCost]
+                costMin = l_1*c[1][i][j] + l_2*c[2][i][j]
+                cost_ij = l_1*c[1][i][indiceDepot_minCost] + l_2*c[2][i][indiceDepot_minCost]
+                if cost_ij < costMin
                     indiceDepot_minCost = j
                 end
             end
         end
-        x[i,indiceDepot_minCost] = 1
+        x[i][indiceDepot_minCost] = 1
     end
 
+
     return x
+end
+
+#Retourne si le kp 2-1 est applicable, et le vecteur y post-kp.
+function kp_2_1(i,j,x,y,f,c,l_1,l_2)
+
+    kp21_feasability = false
+    y_new = deepcopy(y)
+    z_init = zValue(i,j,x,y,f,c)
+ 
+
+    #Listes contenant les indices des site ouverts (1) ou fermées (0).
+    indice_1 = Vector{Int64}(undef,0)
+    indice_0 = Vector{Int64}(undef,0)
+
+    #Initialisation des listes d'indices.
+    for k = 1:j
+        if y[k]==1
+            push!(indice_1,k)
+        else 
+            push!(indice_0,k)
+        end
+    end
+
+    #Si il y a moins de 2 site ouverts, pas de kp 2-1 possible.
+    if (length(indice_1) < 2)
+        return false,y
+    end
+
+    #Recherche d'un kp 2-1.
+    for a = 1:length(indice_1)
+        for b = 1:length(indice_1)
+            for d = 1:length(indice_0)
+                
+                if (a!=b)&&(b!=d)
+
+                    #swapping
+                    y_new[indice_1[a]] = 0
+                    y_new[indice_1[b]] = 0
+                    y_new[indice_0[d]] = 1
+
+                    #println(a,",",b,",",d," : ",y)
+
+                    #évaluation de la nouvelle solution
+                    x_new = pairingClient(i,j,y_new,c,l_1,l_2)
+                    z_new = zValue_pondere(i,j,x_new,y_new,f,c,l_1,l_2)
+
+                    if (z_new < z_init)
+                        kp21_feasability = true
+                        return kp21_feasability,y_new
+                    end
+
+                    #unswapping
+                    y_new[indice_1[a]] = 1
+                    y_new[indice_1[b]] = 1
+                    y_new[indice_0[d]] = 0
+                end
+            end
+        end
+    end
+    kp21_feasability = false
+    return kp21_feasability,y_new
+end
+
+#Retourne si le kp 1-1 est applicable, et le vecteur y post-kp.
+#=function kp_1_1(i,j,x,y,f,c)
+
+    kp11_feasability = false
+    y_new = deepcopy(y)
+    z_init = zValue(i,j,x,y,f,c)
+ 
+
+    #Listes contenant les indices des site ouverts (1) ou fermées (0).
+    indice_1 = Vector{Int64}(undef,0)
+    indice_0 = Vector{Int64}(undef,0)
+
+    #Initialisation des listes d'indices.
+    for k = 1:j
+        if y[k]==1
+            push!(indice_1,k)
+        else 
+            push!(indice_0,k)
+        end
+    end
+
+    #Si il y a moins de 1 site ouverts, pas de kp 1-1 possible.
+    if (length(indice_1) < 1)
+        return false,y
+    end
+
+    #Recherche d'un kp 1-1.
+    for a = 1:length(indice_1)
+        for b = 1:length(indice_0)
+
+            if (a!=b)
+                #swapping
+                y_new[indice_1[a]] = 0
+                y_new[indice_0[b]] = 1
+
+                #println(a,",",b,",",d," : ",y)
+
+                #évaluation de la nouvelle solution
+                x_new = pairingClient(i,j,y_new,c)
+                z_new = zValue(i,j,x_new,y_new,f,c)
+                println(z_new,",",z_init)
+                if (z_new < z_init)
+                    kp21_feasability = true
+                    return kp21_feasability,y_new
+                end
+
+                #unswapping
+                y_new[a] = 1
+                y_new[b] = 0
+
+            end
+            
+        end
+    end
+    println("fin")
+    kp11_feasability = false
+    return kp11_feasability,y_new
+end=#
+
+
+
+
+
+
+function zValue(i,j,x,y,f,c)
+    sum_y = 0
+    for n = 1:j
+        sum_y += y[n]*f[n]
+    end
+    sum_x = 0
+
+    for n = 1:i
+        for m = 1:j
+            sum_x += x[n][m] * c[n][m]
+        end
+    end
+
+    return sum_y+sum_x
+end
+
+function zValue_pondere(i,j,x,y,f,c,l_1,l_2)
+    return (zValue(i,j,x,y,f[1],c[1])*l_1) + (zValue(i,j,x,y,f[2],c[2])*l_2)
 end
--- main.jl
+++ main.jl
@@ -1,6 +1,8 @@
 include("greedyInit.jl")
 include("localSearch.jl")
 
+#import Pkg; Pkg.add("Plots")
+using Plots
 #=Un UFLP : 
 Soit :
     -i le nombre de client 
@@ -22,10 +24,13 @@ function main()
     i = 10
 
     #cout ouverture site
-    f = [10,7,3,11,5,4]
+    f_1 = [10,7,3,11,5,4]
+    f_2 = [3,2,12,9,1,7]
+
+    f = [f_1,f_2]
 
     #cout service client
-    c = [
+    c_1 = [
         [7,2,1,9,7,3],
         [3,2,4,8,2,1],
         [1,2,4,6,1,6],
@@ -38,23 +43,64 @@ function main()
         [7,4,4,7,5,1]
     ]
 
+    c_2 = [
+        [9,7,3,5,8,1],
+        [1,2,6,4,1,9],
+        [6,6,1,1,6,7],
+        [5,4,5,5,2,1],
+        [3,6,4,3,9,1],
+        [1,3,8,6,1,3],
+        [3,4,5,6,8,9],
+        [1,6,1,8,2,3],
+        [3,2,9,6,7,6],
+        [8,3,9,4,5,3]
+    ]
+
+    c = [c_1,c_2]
+    f = [f_1,f_2]
+
     
     #(x_init,y_init) = greedyContstruction(n,m,f,c)
     #Valeur arbitraire pour x et y en attendant l'implémentation de la construction gloutonne
+    
+
     x_init = [
-            [0,0,0,0,1,0,1,1,0,0], #1
-            [0,0,0,0,0,0,0,0,0,0], #0
-            [0,0,0,0,0,0,0,0,0,0], #0
-            [1,0,1,0,0,1,0,0,1,0], #1
-            [0,1,0,1,0,0,0,0,0,1], #1
-            [0,0,0,0,0,0,0,0,0,0], #0
-            ]
+         #y=[1,0,0,1,1,0]
+            [0,0,0,1,0,0],
+            [0,0,0,0,1,0],
+            [0,0,0,1,0,0],
+            [0,0,0,0,1,0],
+            [1,0,0,0,0,0],
+            [0,0,0,1,0,0],
+            [1,0,0,0,0,0],
+            [1,0,0,0,0,0],
+            [0,0,0,1,0,0],
+            [0,0,0,0,1,0]
+        ]
 
     y_init = [1,0,0,1,1,0]
 
-    x_opt,y_opt = localSearch(i,j,y_init,f,c)
+    x_opt_1,y_opt_1 = localSearch(i,j,x_init,y_init,f,c,1,0)
+    x_opt_2,y_opt_2 = localSearch(i,j,x_init,y_init,f,c,1,0)
 
-    
+
+    println("y_init_1 : ",y_init)
+    println("z_init_1 : ",zValue(i,j,x_init,y_init,f,c_1))
+    println("y_opt_1 : ",y_opt_1)
+    println("z_opt_1 : ",zValue(i,j,x_opt_1,y_opt_1,f,c_1))
+
+    println("y_init_2 : ",y_init)
+    println("z_init_2 : ",zValue(i,j,x_init,y_init,f,c_2))
+    println("y_opt_2 : ",y_opt_2)
+    println("z_opt_2 : ",zValue(i,j,x_opt_2,y_opt_2,f,c_2))
+
+
+    #x = 1:10; y = [rand(10),rand(10)];
+    #scatter(x, y)
+
+for i = 1:10
+    println(rand(6))
+end
 
 end
 
